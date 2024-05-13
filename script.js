856a8bea7bac89c19edece971a33107165f5b5c7
NDIV123 <dmarshelano@gmail.com>
2024-05-13 07:24:48 +0700

    inisiasi file

---
+++ script.js
@@ -0,0 +1,51 @@
+// Kelompok JS
+
+const kelompokRoot = document.getElementById("kelompokRoot");
+
+const datas = [
+  {
+    nama: "Riqza Harly Saputra",
+    NPM: "237006055",
+    foto: "https://simak.unsil.ac.id/us-unsil/foto/2023/237006055.jpg",
+  },
+  {
+    nama: "Diva Marshelano Ardentinnova S",
+    NPM: "237006041",
+    foto: "https://simak.unsil.ac.id/us-unsil/foto/2023/237006041.jpg",
+  },
+  {
+    nama: "Widia Senja Rahayu",
+    NPM: "237006024",
+    foto: "https://simak.unsil.ac.id/us-unsil/foto/2023/237006024.jpg",
+  },
+  {
+    nama: "Mohammad Daffa Pradiva Untara",
+    NPM: "237006061",
+    foto: "https://simak.unsil.ac.id/us-unsil/foto/2023/237006061.jpg",
+  },
+  {
+    nama: "Bakti Dwi Pamungkas",
+    NPM: "237006049",
+    foto: "https://simak.unsil.ac.id/us-unsil/foto/2023/237006059.jpg",
+  },
+];
+
+const createCard = (data) => {
+  return `
+  <div class="card w-64 h-[60vh] bg-base-100 shadow-xl">
+  <figure><img class="object-center" src=${data.foto} alt=${data.nama} /></figure>
+  <div class="card-body h-40 flex flex-col items-center justify-around">
+    <h2 class="card-title text-base text-center">${data.nama}</h2>
+    <p class="text-center">${data.NPM}</p>
+  </div>
+  `;
+};
+
+
+window.onload = () => {
+    datas.forEach((data) => {
+      kelompokRoot.innerHTML += createCard(data);
+    });
+}
+
+// Kelompok JS
