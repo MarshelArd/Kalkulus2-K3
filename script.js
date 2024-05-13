685c6aadb0ee539bbee987abfe475b96d7658c1a
baktidwipamungkas <baktidwipamungkas@gmail.com>
2024-05-13 17:53:21 +0700

    Menjalankan fitur slidecard materi

--- script.js
+++ script.js
@@ -49,3 +49,30 @@ window.onload = () => {
 }
 
 // Kelompok JS
+
+//Materi JS
+
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+const slideContainer = document.getElementById('slideContainer');
+
+let slideIndex = 0;
+
+prevBtn.addEventListener('click', () => {
+  slideIndex--;
+  showSlides();
+});
+
+nextBtn.addEventListener('click', () => {
+  slideIndex++;
+  showSlides();
+});
+
+function showSlides() {
+  const slides = slideContainer.getElementsByClassName('cardMateri');
+  if (slideIndex >= slides.length) { slideIndex = 0; }
+  if (slideIndex < 0) { slideIndex = slides.length - 1; }
+  slideContainer.style.transform = `translateX(-${slideIndex * (slides[0].offsetWidth + 20)}px)`;
+}
+
+//Materi JS
