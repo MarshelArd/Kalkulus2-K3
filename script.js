a711963d318b5038fa2dfb5ae6ebc7f0d91619c3
Izaa20 <riqzaharlysaputra18@gmail.com>
2024-05-16 16:41:14 +0700

    mengubah tampilan kelompok

--- script.js
+++ script.js
@@ -2,37 +2,48 @@
 
 const wrapper = document.querySelector(".kelompokwrapper");
 const carousel = document.querySelector(".kelompokcarousel");
-const arrowBtns = document.querySelectorAll(".kelompokwrapper i");
 const firstCardWidth = carousel.querySelector(".kelompokcard").offsetWidth;
+const arrowBtns = document.querySelectorAll(".kelompokwrapper i");
 const carouselChildrens = [...carousel.children];
 
-let isDragging = false, startX, startScrollLeft, timeoutId;
+let isDragging = false, isAutoPlay = true, startX, startScrollLeft, timeoutId;
+
 
 let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
+
 
 carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
     carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
 });
 
+
 carouselChildrens.slice(0, cardPerView).forEach(card => {
     carousel.insertAdjacentHTML("beforeend", card.outerHTML);
 });
 
-arrowBtns.forEach((btn) => {
+
+carousel.classList.add("no-transition");
+carousel.scrollLeft = carousel.offsetWidth;
+carousel.classList.remove("no-transition");
+
+
+arrowBtns.forEach(btn => {
     btn.addEventListener("click", () => {
-        carousel.scrollLeft += btn.id === "left" ? -firstCardWidth : firstCardWidth;
-    })
+        carousel.scrollLeft += btn.id == "left" ? -firstCardWidth : firstCardWidth;
+    });
 });
 
 const dragStart = (e) => {
     isDragging = true;
     carousel.classList.add("dragging");
+    
     startX = e.pageX;
     startScrollLeft = carousel.scrollLeft;
 }
 
 const dragging = (e) => {
-    if(!isDragging) return;
+    if(!isDragging) return; 
+    
     carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
 }
 
@@ -41,35 +52,38 @@ const dragStop = () => {
     carousel.classList.remove("dragging");
 }
 
-const autoPlay = () => {
-    if(window.innerWidth < 800) return;
-    timeoutId = setTimeout(() => carousel.scrollLeft += firstCardWidth, 2500);
-}
-
-// autoPlay();
-
 const infiniteScroll = () => {
+   
     if(carousel.scrollLeft === 0) {
         carousel.classList.add("no-transition");
         carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth);
         carousel.classList.remove("no-transition");
-    } else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth){
+    }
+   
+    else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth) {
         carousel.classList.add("no-transition");
         carousel.scrollLeft = carousel.offsetWidth;
         carousel.classList.remove("no-transition");
     }
 
-    clearTimeout(timeoutId);
-    if(!wrapper.matches(":hover")) autoPlay();
+
+    // clearTimeout(timeoutId);
+    // if(!wrapper.matches(":hover")) autoPlay();
 }
 
+// const autoPlay = () => {
+//     if(window.innerWidth < 800 || !isAutoPlay) return; // Return if window is smaller than 800 or isAutoPlay is false
+//     // Autoplay the carousel after every 2500 ms
+//     timeoutId = setTimeout(() => carousel.scrollLeft += firstCardWidth, 2500);
+// }
+// autoPlay();
 
 carousel.addEventListener("mousedown", dragStart);
 carousel.addEventListener("mousemove", dragging);
 document.addEventListener("mouseup", dragStop);
 carousel.addEventListener("scroll", infiniteScroll);
 wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
-wrapper.addEventListener("mouseleave", autoPlay);
+// wrapper.addEventListener("mouseleave", autoPlay);
 
 // Kelompok JS
 
